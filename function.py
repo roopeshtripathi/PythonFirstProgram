b3aeaf9eccf2fe77a2eaa4b4f84b27910b8e4401
roopeshtripathi <roopesh.tripathi@gmail.com>
2017-04-22 17:54:20 +0530

    My First python program

---
+++ function.py
@@ -0,0 +1,17 @@
+print("Hello ,Roopesh")
+
+def myFirstFunction():
+    myName = input("what is your name ?")
+    myVar = input("Enter number ?")
+
+    if (myName == "Roopesh" or myVar!= 0):
+        print("Roopesh is great!")
+    elif (myName == "Ram"):
+        print("my name is Ram")
+    else:
+        print("Hello There !")
+
+myFirstFunction()
+
+
+
---
+++ myFirstPythonProgram.py
@@ -0,0 +1,3 @@
+print("Hello ,Roopesh")
+myVar = "Hello Roopesh"
+print(myVar)
